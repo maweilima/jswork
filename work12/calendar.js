999a89839cbfc21156928dc08c9e592196479229
maaweilima <2338323618@qq.com>
2019-12-01 21:47:51 +0800

    work12

--- work12/calendar.js
+++ work12/calendar.js
@@ -1,13 +1,13 @@
 function calendar(y) {
-    var w = new Date(y, 0).getDate();
+    var w = new Date(y, 0).getDay();
     var html = '<div class="box">';
-    for(var m = 1; m <= 12; ++m) {
+    for (var m = 1; m <=12; ++m) {
         html += '<table>';
         html += '<tr class="title"><th colspan="7">' + y + '年' + m + '月</th></tr>';
         html += '<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
-        var max = new Date(y, m, 0).getDate();
+        var max = new Date(y, m ,0).getDate();
         html += '<tr>';
-        for (var d = 1;d <= max; ++d) {
+        for (var d = 1; d <= max; ++d) {
             if (w && d == 1) {
                 html += '<td colspan="' + w + '"> </td>';
             }
@@ -19,8 +19,8 @@ function calendar(y) {
             }
             w = (w + 1 > 6) ? 0 : w + 1;
         }
-        html +='</table>';
+        html += '</table>';
     }
-    html +='</div>';
+    html += '</div>';
     return html;
 }
